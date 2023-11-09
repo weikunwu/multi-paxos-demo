bab5f8d5a39f59d823301cd9cfa6af561ed1d777
Shanglin Mo <shanglin_mo@outlook.com>
2023-11-09 15:36:37 -0600

    Modified packet attributes

--- webapp/src/objects/Packet.js
+++ webapp/src/objects/Packet.js
@@ -1,7 +1,9 @@
 class Packet {
 	constructor(from, to) {
+		this.type = null; // packet type, prepare, proposal, accept;
+		this.value = null; // packet value;
 		this.id = null; // packet ID, must be unique;
-		this.time = Date.now(); // packet timestamp;
+		this.proposalNum = Date.now(); // packet timestamp;
 		this.from = from; // server that send this packet;
 		this.to = to; // server that this packet is sent to;
 	}
