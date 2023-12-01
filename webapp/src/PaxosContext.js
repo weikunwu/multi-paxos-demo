c8d0b9522d64a8205a6c391eec58f2aff0448914
Minjae <huna629@gmail.com>
2023-12-01 00:54:43 -0600

    asdf

--- webapp/src/PaxosContext.js
+++ webapp/src/PaxosContext.js
@@ -3,6 +3,8 @@ import {
   useState,
 } from 'react';
 
+import { Server } from './objects/Server';
+
 const PaxosContext = createContext();
 
 const PaxosProvider = ({ children }) => {
@@ -10,9 +12,11 @@ const PaxosProvider = ({ children }) => {
     on: false, // Determine whether simulation is on or paused
     speed: 2, // Speed must be between 1 - 5, which determine the speed of simulation
     dropRate: 0, // Possibility that a message is dropped
-    tab: 'playground',
+      tab: 'playground',
     scenario: 'fault1',
-    servers: [], // A list of server objects
+    servers: [
+      new Server('1'),
+    ], // A list of server objects
     packets: [] // A list of packets objects
   })
 
