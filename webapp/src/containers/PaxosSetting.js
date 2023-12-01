26a7d9abd7890e82e41ca7cb6a364c7020852ac7
Minjae <huna629@gmail.com>
2023-12-01 00:47:58 -0600

    modified button

--- webapp/src/containers/PaxosSetting.js
+++ webapp/src/containers/PaxosSetting.js
@@ -22,59 +22,6 @@ const PaxosSetting = ({ className, faultMode }) => {
 function timeout(delay) {
   return new Promise(res => setTimeout(res, delay));
 }
-const failure1 = async () => {
-  handleStartButton();
-
-  const newServers = [];
-  for (let i = 0; i < 5; i++) {
-    newServers.push(new Server(`${i + 1}`));
-  }
-
-  const circleRadius = 200; 
-  const offset = 200; 
-  const totalServers = newServers.length;
-  const angle = 2 * Math.PI / totalServers;
-
-  newServers.forEach((server, i) => {
-    const theta = angle * (i + 1); 
-    server.x = offset + circleRadius * Math.cos(theta) - 10;
-    server.y = offset + circleRadius * Math.sin(theta) - 10;
-  });
-
-  await setPaxosState((prevState) => ({
-    ...prevState,
-    servers: newServers
-  }));
-
-  await timeout(0);
-
-  setPaxosState((prevState) => {
-    const updatedServers = prevState.servers;
-    const newPackets = [
-      ...prevState.packets,
-      ...updatedServers[0].broadcastPrepare(['2', '3'], 20),
-    ];
-
-    return {
-      ...prevState,
-      packets: newPackets
-    };
-  });
-
-  await timeout(4900);
-
-  setPaxosState((prevState) => {
-    const updatedServers = prevState.servers;
-    const additionalPackets = [
-      ...updatedServers[4].broadcastPrepare(['1', '3'], 10),
-    ];
-
-    return {
-      ...prevState,
-      packets: [...prevState.packets, ...additionalPackets]
-    };
-  });
-};
 
 const failure6 = async () => {
   handleStartButton();
@@ -174,43 +121,20 @@ const failure6 = async () => {
 
   return (
     <div className={`paxos-setting-container ${className}`} >
-      {
-        faultMode && (
+      {faultMode ?
+        <div>
           <Button
             className='add-button'
             type='primary'
-            onClick={failure1} // Assuming you have a function for Failure 1
-          >
-            Failure 1
-          </Button>
-        )
-      }
-      {
-        faultMode && (
-          <Button
-            className='add-button'
-            type='primary'
-            onClick={failure6}
-          >
-            Failure 6
-          </Button>
-        )
-      }
-
-      {
-        !faultMode &&
-        <>
-          <Button
-            className='add-button'
-            type='primary'
-            disabled={
-              paxosState.servers.length >= 10 || paxosState.on || paxosState.packets.length > 0
-            } 
-            onClick={handleAddServer}
-          >
-            Add Node
-          </Button>
-        </>
+            onClick={failure6}>Not Updating MinProposal</Button>
+        </div>
+        :
+        <Button
+          className='add-button'
+          type='primary'
+          disabled={
+            paxosState.servers.length >= 10 || paxosState.on || paxosState.packets.length > 0
+          } onClick={handleAddServer}>Add Node</Button>
       }
       <Button
         className='start-button'
